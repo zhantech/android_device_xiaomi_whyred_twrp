1f51170f3f60b3b4e0771168697aa03bd0fcb654
ZHANtech™ <hifzhan41@gmail.com>
2020-06-01 11:10:17 +0700

    whyred: inject build date

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -63,6 +63,11 @@ TARGET_PREBUILT_KERNEL := device/xiaomi/whyred/prebuilt/Image.gz-dtb
 TARGET_BOARD_PLATFORM := sdm660
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno509
 
+#Init
+TARGET_INIT_VENDOR_LIB := libinit_whyred
+TARGET_RECOVERY_DEVICE_MODULES := libinit_whyred
+TARGET_PLATFORM_DEVICE_BASE := /devices/soc/
+
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 262144
 
@@ -102,4 +107,3 @@ TW_USE_TOOLBOX := true
 
 # Batik Official
 BR_OFFICIAL := true
-
