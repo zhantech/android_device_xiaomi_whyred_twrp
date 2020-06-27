cf9de0bfb3a978a7ba105658c0efcf81f719fc40
ZHANtech™ <hifzhan41@gmail.com>
2020-06-27 08:20:03 +0700

    Change omni to batik

--- omni_whyred.mk
+++ omni_whyred.mk
@@ -20,7 +20,7 @@ PRODUCT_RELEASE_NAME := whyred
 $(call inherit-product, build/target/product/embedded.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/batik/config/common.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := whyred
